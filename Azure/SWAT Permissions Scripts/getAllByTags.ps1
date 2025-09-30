b7dbbc8be200c90c9c04c42dd9ec0cc4a7c6c04f
TomSimpson <thomas.simpson@broadsign.com>
2025-09-30 14:17:47 +0200

    [ACR-1421] - Get by tag script (draft)

---
+++ Azure/SWAT Permissions Scripts/getAllByTags.ps1
@@ -0,0 +1,26 @@
+# Connect if not connected already
+# Connect-AzAccount
+
+# Get all resources in the subscription
+$resources = Get-AzResource
+
+# Group by tags
+    # Need to group by 'OwnedByTeam still'
+$grouped = $resources | Where-Object { $_.Tags } | ForEach-Object {
+    foreach ($tag in $_.Tags.GetEnumerator()) {
+        [PSCustomObject]@{
+            TagKey   = $tag.Key
+            TagValue = $tag.Value
+            Resource = $_.ResourceId
+        }
+    }
+} | Group-Object TagKey, TagValue
+
+# Print grouped resources
+foreach ($group in $grouped) {
+    Write-Output "`nTag: $($group.Name)"
+    $group.Group | ForEach-Object { Write-Output "  - $($_.Resource)" }
+}
+
+
+OWNED BY TEAM
